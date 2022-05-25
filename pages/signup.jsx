4e9d26f6c25b93d5330cf7de3e3b1203321d1c1e
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-05-25 14:25:47 +0900

    ✨ サインアップフォームのコンポーネントを実装

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,4 +1,6 @@
+import { PasswordInput, TextInput } from '@mantine/core'
 import Head from 'next/head'
+import { At, Ballpen, Key } from 'tabler-icons-react'
 
 export default function Home() {
   return (
@@ -6,7 +8,39 @@ export default function Home() {
       <Head>
         <title>Signup Page</title>
       </Head>
-      <h1>Signup Page</h1>
+      <h1>メールアドレスだけで簡単に登録できます</h1>
+      <TextInput
+        id='name-input'
+        placeholder='Your name'
+        label='Full name'
+        aria-label='Full name'
+        size='lg'
+        required
+        icon={<Ballpen size={16} />}
+        // error='error message'
+        // rightSection={<Loader size='xs' />}
+      />
+      <TextInput
+        id='email-input'
+        placeholder='Your email'
+        label='Email'
+        aria-label='Email'
+        size='lg'
+        required
+        icon={<At size={16} />}
+        // error='error message'
+        // rightSection={<Loader size='xs' />}
+      />
+      <PasswordInput
+        id='password-input'
+        placeholder='Password'
+        label='Password'
+        description='At least 6'
+        size='lg'
+        required
+        icon={<Key size={16} />}
+        // error='error message'
+      />
     </div>
   )
 }
