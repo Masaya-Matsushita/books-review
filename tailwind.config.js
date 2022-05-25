1d98aa09c2b2b644c9f53faab32308b0ffa47adf
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-05-25 11:40:28 +0900

    ✨ Tailwind CSSとMantineを併用するための構成を設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    // Mantineと一致させる https://mantine.dev/theming/responsive/
+    screens: {
+      xs: '576px',
+      sm: '768px',
+      md: '992px',
+      lg: '1200px',
+      xl: '1400px',
+    },
     extend: {},
   },
   plugins: [],
