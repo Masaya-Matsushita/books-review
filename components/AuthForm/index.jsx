f903d5548c48e66ad9116ae6d859a5039082b3e6
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-05-28 19:36:27 +0900

    ✨ pathnameによってAuthFormの挙動を切り替え

--- components/AuthForm/index.jsx
+++ components/AuthForm/index.jsx
@@ -3,8 +3,10 @@ import { At, Ballpen, Book2, Key } from 'tabler-icons-react'
 import Link from 'next/link'
 import { useStateManagement } from 'hooks/useStateManagement'
 import { useAuthFormInitialize } from 'hooks/useAuthFormInitialize'
+import { useRouter } from 'next/router'
 
 export const AuthForm = (props) => {
+  const router = useRouter()
   const form = useAuthFormInitialize()
   const { state, dispatch } = useStateManagement()
 
@@ -29,6 +31,50 @@ export const AuthForm = (props) => {
     } catch (error) {
       dispatch({ type: 'error', error })
     }
+  }
+
+  if (router.pathname === '/signin') {
+    return (
+      <div>
+        <h1>{props.title}</h1>
+        <Box sx={{ maxWidth: 400 }} mx='auto'>
+          <form onSubmit={form.onSubmit(handleSubmit)}>
+            <TextInput
+              id='email'
+              placeholder='example@mail.com'
+              label='Email'
+              aria-label='Email'
+              size='lg'
+              required
+              icon={<At size={16} />}
+              {...form.getInputProps('email')}
+              className='mt-2'
+            />
+            <PasswordInput
+              id='password'
+              placeholder='半角英数6文字以上'
+              label='Password'
+              size='lg'
+              required
+              icon={<Key size={16} />}
+              {...form.getInputProps('password')}
+              className='mt-2'
+            />
+            <Group position='right' mt='xl'>
+              <Button type='submit' size='lg' leftIcon={<Book2 size={16} />}>
+                {props.submitText}
+              </Button>
+            </Group>
+          </form>
+        </Box>
+        <Link href={props.linkHref}>
+          <a>{props.linkText}</a>
+        </Link>
+        {state.loading ? <div>ローディング中</div> : null}
+        {state.error ? <div>{state.error.message}</div> : null}
+        <div>{state.data}</div>
+      </div>
+    )
   }
 
   return (
--- hooks/useAuthFormInitialize.jsx
+++ hooks/useAuthFormInitialize.jsx
@@ -1,18 +1,31 @@
 import { z } from 'zod'
 import { useForm, zodResolver } from '@mantine/form'
-
-const schema = z.object({
-  name: z
-    .string()
-    .min(2, { message: '2文字以上10文字以内で入力してください。' })
-    .max(10, { message: '2文字以上10文字以内で入力してください。' }),
-  email: z.string().email({ message: '正しく入力されていません。' }),
-  password: z.string().regex(/^(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}$/i, {
-    message: '「半角英数それぞれ1種類以上を含む6文字以上」で入力してください。',
-  }),
-})
+import { useRouter } from 'next/router'
 
 export const useAuthFormInitialize = () => {
+  const router = useRouter()
+
+  const schema =
+    router.pathname === '/signup'
+      ? z.object({
+          name: z
+            .string()
+            .min(2, { message: '2文字以上10文字以内で入力してください。' })
+            .max(10, { message: '2文字以上10文字以内で入力してください。' }),
+          email: z.string().email({ message: '正しく入力されていません。' }),
+          password: z.string().regex(/^(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}$/i, {
+            message:
+              '「半角英数それぞれ1種類以上を含む6文字以上」で入力してください。',
+          }),
+        })
+      : z.object({
+          email: z.string().email({ message: '正しく入力されていません。' }),
+          password: z.string().regex(/^(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}$/i, {
+            message:
+              '「半角英数それぞれ1種類以上を含む6文字以上」で入力してください。',
+          }),
+        })
+
   const form = useForm({
     schema: zodResolver(schema),
     initialValues: {
