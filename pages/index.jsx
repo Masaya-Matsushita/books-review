700c027d85460b5bb50db42174c152c44924c6e1
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-05-29 10:49:55 +0900

    ✨ token認証で書籍一覧を取得

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,45 @@
 import { HeadComponent as Head } from 'components/Head'
 import { TokenContext } from 'pages/_app'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 
 export default function Home() {
   const { token } = useContext(TokenContext)
+  const [posts, setPosts] = useState([])
+
+  const getPosts = async (token) => {
+    try {
+      const res = await fetch(
+        'https://api-for-missions-and-railways.herokuapp.com/books?offset=0',
+        {
+          method: 'GET',
+          mode: 'cors',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      if (!res.ok) {
+        throw new Error('エラー')
+      }
+      const json = await res.json()
+      setPosts(json)
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
 
   return (
     <div>
       <Head title='index page' />
       <h1>Index Page</h1>
-      {token ? <div>{token}</div> : null}
+      <button onClick={token ? (e) => getPosts(token, e) : null}>ボタン</button>
+      {posts ? (
+        <ul>
+          {posts.map((post) => {
+            return <li key={post.id}>{post.title}</li>
+          })}
+        </ul>
+      ) : null}
     </div>
   )
 }
