486877083cca220210559a5aa09c2a90d29a613a
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-06-03 15:14:03 +0900

    ✨ ログアウト処理を実装

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,6 +1,18 @@
+import { Menu } from '@mantine/core'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useCookies } from 'react-cookie'
+import { Logout, Settings } from 'tabler-icons-react'
 
 export const Header = ({ state }) => {
+  const [cookies, setCookie, removeCookie] = useCookies(['token'])
+  const router = useRouter()
+
+  const logout = () => {
+    removeCookie('token')
+    router.push('/signin')
+  }
+
   if (state.error) {
     return <div className='flex justify-end'>{state.error}</div>
   }
@@ -9,9 +21,17 @@ export const Header = ({ state }) => {
     return (
       <div className='flex justify-end'>
         ログイン済：
-        <Link href='/profile'>
-          <a>{state.name}</a>
-        </Link>
+        <Menu control={<span>{state.name}</span>}>
+          <Menu.Label>Menu</Menu.Label>
+          <Menu.Item icon={<Settings size={14} />}>
+            <Link href='/profile'>
+              <a>User Name</a>
+            </Link>
+          </Menu.Item>
+          <Menu.Item color='red' icon={<Logout size={14} />} onClick={logout}>
+            Logout
+          </Menu.Item>
+        </Menu>
         さん
       </div>
     )
