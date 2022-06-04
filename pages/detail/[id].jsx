b1b6820b019c7922d850dc3a41c6fa3c43513e3f
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-06-04 13:54:41 +0900

    ✨ useDetailStateで書籍レビュー詳細画面を状態管理

--- pages/detail/[id].jsx
+++ pages/detail/[id].jsx
@@ -1,3 +1,4 @@
+import { useDetailState } from 'hooks/useDetailState'
 import { useRouter } from 'next/router'
 import { useCallback, useEffect } from 'react'
 import { useCookies } from 'react-cookie'
@@ -5,25 +6,31 @@ import { useCookies } from 'react-cookie'
 export default function DetailId() {
   const router = useRouter()
   const [cookies, setCookie, removeCookie] = useCookies()
+  const { state, dispatch } = useDetailState()
 
   const getDetail = useCallback(async () => {
-    const res = await fetch(
-      `https://api-for-missions-and-railways.herokuapp.com/books/${router.query.id}`,
-      {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: `Bearer ${cookies.token}`,
-        },
-      }
-    )
-    const json = await res.json()
-    console.log(json)
-  }, [cookies.token, router.query.id])
+    try {
+      const res = await fetch(
+        `https://api-for-missions-and-railways.herokuapp.com/books/${router.query.id}`,
+        {
+          method: 'GET',
+          headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${cookies.token}`,
+          },
+        }
+      )
+      const json = await res.json()
+      // console.log(json)
+      dispatch({ type: 'end', data: json })
+    } catch (error) {
+      dispatch({ type: 'error', error })
+    }
+  }, [cookies.token, router.query.id, dispatch])
 
   useEffect(() => {
-    getDetail()
-  }, [getDetail])
+    router.query.id ? getDetail() : null
+  }, [router.query.id, getDetail])
 
-  return <div>{router.query.id}</div>
+  return <div>{state.data ? state.data.id : null}</div>
 }
