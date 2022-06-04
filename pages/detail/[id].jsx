80d92da1c55957d6cfa1c04670e0e553a7081e4a
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-06-04 14:56:14 +0900

    ✨ 書籍レビュー詳細画面の表示を実装

--- pages/detail/[id].jsx
+++ pages/detail/[id].jsx
@@ -1,3 +1,4 @@
+import { Button, Card, Loader } from '@mantine/core'
 import { useDetailState } from 'hooks/useDetailState'
 import { useRouter } from 'next/router'
 import { useCallback, useEffect } from 'react'
@@ -32,5 +33,30 @@ export default function DetailId() {
     router.query.id ? getDetail() : null
   }, [router.query.id, getDetail])
 
-  return <div>{state.data ? state.data.id : null}</div>
+  return (
+    <div className='bg-slate-100'>
+      {state.data ? (
+        <div>
+          <Card>
+            <h1>{state.data.title}</h1>
+            <h3>{state.data.detail}</h3>
+            <p>{state.data.review}</p>
+            <a href={state.data.url} className='block mr-4 text-right'>
+              作品のリンク
+            </a>
+            <p className='mr-4 mb-0 text-right'>
+              Reviewed by {state.data.reviewer}
+            </p>
+          </Card>
+          <Button
+            size='lg'
+            className='block mt-4 mr-4 ml-auto'
+            onClick={() => router.push('/')}
+          >
+            一覧へ戻る
+          </Button>
+        </div>
+      ) : null}
+    </div>
+  )
 }
